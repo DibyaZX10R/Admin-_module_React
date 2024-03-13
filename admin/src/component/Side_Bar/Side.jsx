67e0949291ccd695fe2e5468a2b862cbb11914ab
Dibya <dibyadarshirout@gmail.com>
2024-03-13 19:35:09 +0530

    19:35

--- admin/src/component/Side_Bar/Side.jsx
+++ admin/src/component/Side_Bar/Side.jsx
@@ -11,25 +11,39 @@ const Side = ({ onMenuItemClick }) => {
         </div>
         <ul className="menu-container" id="maincontent">
           <li className="menu-item active ">
-            <Link to={`/`} className="menu-link" onClick={() => onMenuItemClick(' Dashboard')}>
+            <Link to={`/`} className="menu-link" onClick={() => onMenuItemClick('Dashboard')}>
               <i className="fas fa-tachometer-alt"></i><span>Dashboard</span>
             </Link>
           </li>
           <li className="menu-item">
-            <Link to={`/Project`} className="menu-link" onClick={() => onMenuItemClick(' Project Master')}>
+            <Link to={`/Project`} className="menu-link" onClick={() => onMenuItemClick('Project Master')}>
               <i className="fas fa-project-diagram"></i><span>Project Master</span>
             </Link>
           </li>
-		  <li class="menu-item"><Link to={`/Allocation`} class="menu-link" onClick={() => onMenuItemClick('Employee Allocation')}>
-					<i class="fas fa-user"></i><span>Employee Allocation</span></Link></li>
-			<li class="menu-item"><Link to={`/Bench`} class="menu-link" onClick={() => onMenuItemClick(' Bench Employees')}>
-					<i class="fas fa-user-clock"></i><span>Bench Employees</span></Link></li>
-			<li class="menu-item "><Link to={`/History_main`} class="menu-link" onClick={() => onMenuItemClick(' History')}>
-					<i class="fas fa-history"></i><span>History</span></Link></li>
-			<li class="menu-item"><Link to={`/Time`} class="menu-link" onClick={() => onMenuItemClick(' Timesheet')}>
-					<i class="fas fa-clock"></i><span>Timesheet</span></Link></li>
-          {/* Add other menu items */}
+          <li className="menu-item">
+            <Link to={`/Allocation`} className="menu-link" onClick={() => onMenuItemClick('Employee Allocation')}>
+              <i className="fas fa-user"></i><span>Employee Allocation</span>
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to={`/Bench`} className="menu-link" onClick={() => onMenuItemClick('Bench Employees')}>
+              <i className="fas fa-user-clock"></i><span>Bench Employees</span>
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to={`/History_main`} className="menu-link" onClick={() => onMenuItemClick('History')}>
+              <i className="fas fa-history"></i><span>History</span>
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link to={`/Time`} className="menu-link" onClick={() => onMenuItemClick('Timesheet')}>
+              <i className="fas fa-clock"></i><span>Timesheet</span>
+            </Link>
+          </li>
         </ul>
+        <div className="sign-out-container">
+          <button className="sign-out-button">Sign Out</button>
+        </div>
       </div>
     </div>
   );
