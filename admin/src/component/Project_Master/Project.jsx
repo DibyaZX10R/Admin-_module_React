a898940b093a2beabce1c12fc05f2e59ac2f90d9
Dibya <dibyadarshirout@gmail.com>
2024-03-13 17:31:51 +0530

    17:31

--- admin/src/component/Project_Master/Project.jsx
+++ admin/src/component/Project_Master/Project.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import './Project.css';
-
+ 
 const Project = () => {
     const handleEnter = (event) => {
         // Define handleEnter function logic here
     };
-
+ 
     const handleDropdownChange = (event) => {
         // Define handleDropdownChange function logic here
     };
-
+ 
     return (
-        <div className="project-container">
-            <form className="project-form">
+        <div>
+            {/* <h2 className="title">Project Master Details:</h2> */}
+            <form className="project-container">
                 <div className="project-field">
-                    <label htmlFor="projectId">Search by Project ID</label>
-                    <input type="text" id="projectId" name="projectId" placeholder="Search by Project ID" onKeyDown={handleEnter} />
-                
-                
-                    <label htmlFor="projectTitle">Search by Project Title</label>
-                    <input type="text" id="projectTitle" name="projectTitle" placeholder="Search by Project Title" onKeyDown={handleEnter} />
-                
-            
-                    <label htmlFor="projectType">Project Type</label>
-                    <select id="projectType" name="projectType" onChange={handleDropdownChange}>
-                        <option value="" disabled>Select project type</option>
-                        <option value="Billable">Billable</option>
-                        <option value="Non-Billable">Non-Billable</option>
-                    </select>
-                
-                    <label htmlFor="client">Search by Client</label>
-                    <input type="text" id="client" name="client" placeholder="Search by Client" onKeyDown={handleEnter} />
-                </div>  
+                    <label htmlFor="empId">Search by Project ID</label>
+                    <input type="text" id="empId" name="empId" placeholder="Search by Project ID" onKeyDown={handleEnter} />
+                    <label htmlFor="dept">Search by Project Title</label>
+                    <input type="text" id="dept" name="dept" placeholder="Search by Project Title" onKeyDown={handleEnter} />
+                </div>
+                <div className="project-field1">
+                    <div className="Project-container1">
+                        <label htmlFor="projectType">Project Type</label>
+                        <select id="projectType" name="projectType" onChange={handleDropdownChange}>
+                            <option value="" disabled>Select project type</option>
+                            <option value="Billable">Billable</option>
+                            <option value="Non-Billable">Non-Billable</option>
+                        </select>
+                        <label htmlFor="client">Search by client</label>
+                        <input type="text" id="client" name="client" placeholder="Search by client" onKeyDown={handleEnter} />
+                    </div>
+                </div>
+               
             </form>
-            
-            <table className="project-table">
-            <button className='probtn' type="button">Add New</button>
+            <div className="navigation">
+                    <button type="button">Add New</button>
+                </div>
+            <table className='project-table'>
                 <thead>
                     <tr>
                         <th>Project Manager</th>
@@ -49,9 +51,13 @@ const Project = () => {
                         <th>Action</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {/* Add table body content */}
+                </tbody>
             </table>
+           
         </div>
     );
 };
-
+ 
 export default Project;
